f34237e9ad3a4f37e2d5d3c1040529e19edfabbb
Kodjo Adanledji <kad@MACLT99.local>
2020-03-19 17:57:16 +0100

    Need to change APP.js

---
+++ src/components/App.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import AddAppointments from './AddAppointments';
+import ListAppointments from './ListAppointments';
+import SearchAppointments from './SearchAppointments';
+import {without} from 'lodash';
+import '../css/App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      myAppointments: [],
+      formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'dsc',
+      queryText:'',
+      lastIndex: 0
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+    this.searchApts = this.searchApts.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+  }
+  changeOrder(order, dir){
+    this.setState({
+      orderBy: order,
+      orderDir: dir
+    });
+  }
+  addAppointment(apt){
+    let tempAts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempAts.unshift(apt);
+    this.setState({
+      myAppointments: tempAts,
+      lastIndex: this.state.lastIndex + 1
+    });
+  }
+  deleteAppointment(apt){
+    let tempApts = this.state.myAppointments;
+    tempApts= without(tempApts,apt);
+
+    this.setState({
+      myAppointments: tempApts
+    });
+  }
+
+  toggleForm(){
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    });
+  }
+
+  searchApts(query){
+    this.setState({queryText: query});
+  }
+
+  componentDidMount(){
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1})
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+    });
+  }
+
+  render() {
+    let order;
+    let filteredApts=this.state.myAppointments;
+    if(this.state.orderDir === 'asc'){
+      order = 1;
+    } else{
+      order= -1;
+    }
+    filteredApts = filteredApts.sort((a,b)=>{
+      if(a[this.state.orderBy].toLowerCase() < 
+        b[this.state.orderBy].toLowerCase()
+      ){
+        return -1 * order;
+      }else{
+        return 1 * order;
+      }
+    })
+    .filter(eachItem => {
+      return (
+        eachItem['petName']
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase()) || 
+        eachItem['owneName']
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase()) || 
+        eachItem['aptNotes']
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase()) 
+      );
+    });
+    const listItem =this.state.myAppointments.map(item =>(
+    <div>
+      <div>{item.petName}</div>
+      <div>{item.ownerName}</div>
+    </div>
+    ))
+    return (
+      <main className="page bg-white" id="petratings">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 bg-white">
+                <div className="container">
+                    <AddAppointments
+                      formDisplay={this.state.formDisplay}
+                      toggleForm={this.toggleForm}
+                      addAppointment={this.addAppointment}
+                      />
+                    <SearchAppointments 
+                      orderBy={this.state.orderBy}
+                      orderDir={this.state.orderDir}
+                      changeOrder={this.changeOrder}
+                      searchApts={this.searchApts}
+                      />
+                    <ListAppointments appointments={filteredApts} 
+                      deleteAppointment={this.deleteAppointment}/>
+                 </div>   
+                </div>
+              </div>
+            </div>
+        </main>
+    );
+  }
+}
+
+
+export default App;
